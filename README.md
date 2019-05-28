fb54aeb7f0194efea3dee740197f483f008cabd5
RocMark <onigir77799@gmail.com>
2019-05-28 18:41:50 +0800

    <feat> JS30 ArrayCardio UI,JS Upadte
    主要更動
    1. JS30 Day9 Console JS完成
    2. JS30 Day4&7 ArrayCardio UI 70%
    3. JS30 Day4&7 ArrayCardio JS 50%
    4. 更新 README

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # RocMarkSite
-面試用作品
-
+面試用作品 (完整預覽圖於最底部)
 [GithubPageLink](https://rocmark.github.io/ResumeWork/)
+
+## 目的
+- 補足HTML知識點不足
+Ex: MetaTag,Outline,語意化
+- 接觸SEO文章及工具並實踐
+
+- 練習手刻CSS樣式
+- 練習BS4 Grid排版
+- 練習自行建立SASS系統
+- 接觸一些CSS3動畫效果
+
+- 練習原生JS 以及 基礎jQuery
+並進行兩者比較，作為複習之前所學並撰寫文章
+[IT邦 個人撰寫文章列表](https://ithelp.ithome.com.tw/users/20104135/articles)
+
+- 接觸一些相關工具
+Ex: ESLint,ChromeDev
+
+## Preview
+
+![網站首頁](https://i.imgur.com/C75MVuS.png)
+<!-- ![購物清單]() -->
+<!-- ![個人履歷]() -->
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-console */
+
 (function () {
   document.addEventListener('DOMContentLoaded', () => {
-
-
     /* Day1 DrumKit */
-    window.addEventListener('keydown', keyBoxEvent)
+    // window.addEventListener('keydown', keyBoxEvent)
     function keyBoxEvent(e) {
       let keyPressed = e.keyCode
       let target = document.querySelector(`.b-key[data-key="${keyPressed}"]`)
@@ -16,60 +16,256 @@
     /* Day3 CSS Variable */
 
     /* Day4 & Day7 Array Cardio */
-    
+    const arrayCardio = {
+      dom: {
+        originDataDOM: document.querySelector('#b-array__table--origin'),
+        processedDataDOM: document.querySelector('#b-array__table--processed'),
+        btnParent: document.querySelector('.b-array__btns'),
+      },
+      describe: {
+        filter: "篩選出誰生於1500's",
+        sortAge: '以年齡排序 高到低',
+        sortAlpha: '依據 first 做英文字母排序 Z-A',
+        find: '找出第一個叫Albert的人',
+      },
+      originData: [
+        {
+          first: 'Albert',
+          last: 'EinsteinA',
+          birth: 1879,
+          passed: 1955,
+        },
+        {
+          first: 'Albert',
+          last: 'EinsteinB',
+          birth: 1879,
+          passed: 1955,
+        },
+        {
+          first: 'Isaac',
+          last: 'Newton',
+          birth: 1643,
+          passed: 1727,
+        },
+        {
+          first: 'Galileo',
+          last: 'Galilei',
+          birth: 1564,
+          passed: 1642,
+        },
+        {
+          first: 'Marie',
+          last: 'Curie',
+          birth: 1867,
+          passed: 1934,
+        },
+      ],
+      // 掛載監聽 & Render 原始資料
+      init() {
+        this.dom.btnParent.addEventListener('click', this.createTable)
+        
+        this.renderData(
+          arrayCardio.dom.originDataDOM,
+          'origin',
+          arrayCardio.processedData(),
+        )
+      },
+      // 已map後資料
+      processedData() {
+        return this.originData.map(el => ({
+          first: el.first,
+          last: el.last,
+          birth: el.birth,
+          passed: el.passed,
+          age: el.passed - el.birth,
+        }))
+      },
+      // array處理過程
+      processFunc(title) {
+        let result = []
+        // Map 使用
+        let data = arrayCardio.processedData()
+
+        switch (title) {
+          case 'filter':
+            result = data.filter(el => el.birth >= 1500 && el.birth < 1600)
+            break
+          case 'sortAge':
+            result = data.sort((elA, elB) => {
+              let livedA = elA.passed - elA.birth
+              let livedB = elB.passed - elB.birth
+              return livedA < livedB ? 1 : -1
+            })
+            break
+          case 'sortAlpha':
+            result = data.sort((elA, elB) => {
+              let nameA = elA.first.toLowerCase()
+              let nameB = elB.first.toLowerCase()
+              return nameA < nameB ? 1 : -1
+            })
+            break
+          case 'find':
+            result = (function () {
+              let tmpArr = []
+              let target = data.find(elem => elem.first === 'Albert')
+              tmpArr.push(target)
+              return tmpArr
+            }())
+            break
+          default:
+            break
+        }
+
+        return result
+      },
+      // 產生結果陣列
+      createTable(e) {
+        const tagName = e.target.tagName.toLowerCase()
+        if (tagName === 'button') {
+          let { func } = e.target.dataset
+          arrayCardio.renderData(
+            arrayCardio.dom.processedDataDOM,
+            func,
+            arrayCardio.processFunc(func),
+          )
+        }
+      },
+      renderData(target, title, data) {
+        // console.table(title, data)
+        // 處理describe說明文字
+        let describeText = ''
+        switch (title) {
+          case 'filter':
+            describeText = this.describe.filter
+            break
+          case 'sortAge':
+            describeText = this.describe.sortAge
+            break
+
+          case 'sortAlpha':
+            describeText = this.describe.sortAlpha
+            break
+
+          case 'find':
+            describeText = this.describe.find
+            break
+
+          default:
+            break
+        }
+        // dataMarkUp 為陣列資料處理
+        // markUP 為整個Table渲染
+        let dataMarkUp = data.reduce((sum, el) => {
+          let tempStr = `
+            <tr>
+              <th scope="row">1</th>
+              <td>${el.first}</td>
+              <td>${el.last}</td>
+              <td>${el.birth}</td>
+              <td>${el.passed}</td>
+              <td>${el.age}</td>
+            </tr>
+          `
+          return sum + tempStr
+        }, '')
+
+        // console.log(dataMarkUp)
+        
+        let markup = `
+          ${describeText}
+          <table class="b-array__table table table-dark table-bordered table-hover table-sm">
+            <thead class="thead-dark">
+              <tr>
+                <th style="width: 10%" scope="row">${title}</th>
+                <th scope="row">first</th>
+                <th scope="row">last</th>
+                <th scope="row">birth</th>
+                <th scope="row">passed</th>
+                <th scope="row">age</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                ${dataMarkUp}
+              </tr>
+            </tbody>
+          </table>
+        `
+        target.innerHTML = markup
+      },
+    }
+    arrayCardio.init()
+
     /* Day5 Flex Panel Gallery */
 
     /* Day6 TypeAhead */
-    
+
     /* Day8 HTML Canvas */
 
-    /* Day9 DevToolsDomination */ 
-
-    const consoleGenBtn = document.querySelector('.b-console__btn--generate')
-    consoleGenBtn.addEventListener('click', generateConsole)
-
-    // 清空 console 
-    const consoleClearBtn = document.querySelector('.b-console__btn--clear')
-    consoleClearBtn.addEventListener('click', () => { console.clear() })
-    
-    function generateConsole() {
-      // 帶樣式
-      console.log('%c JS30 Day9 Console 練習', 'font-size:25px; background: #212529; color: #fff;')
-      
-      // 一般常見
-      console.log('log', 'Day9 Console')
-      console.info('info', 'Style Work On FireFox')
-      console.warn('warn', 'Day9 Console')
-      console.error('error', 'Day9 Console ')
-          
-      // Interpolated  插入變數
-      // %s 表 string, %d或%i 表整數 %f表浮點數
-      console.log('Interpolated a %s string!', '💩')
-
-      // Testing 用
-      // 判斷地一個參數是否為true, false則噴錯
-      // console.assert((1 > 2), '錯誤訊息: 1並非大於2')
-
-      // console.dir 把物件展開
-      const consoleDemoObj = {
-        title: 'JS30 Day9 Console',
-      }
-      console.dir(consoleDemoObj)
-
-      // console.table 可作欄位篩選
-      // Array 用 Table 形式呈現
-      const consoleDemoArray = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }]
-      console.table(consoleDemoArray, 'name')
-
-      // timing 回傳
-      console.time('fetching data')
-      fetch('https://api.github.com/users/wesbos')
-        .then(data => data.json())
-        .then((data) => { console.timeEnd('fetching data') })
+    /* Day9 DevToolsDomination */
+    const myConsole = {
+      consoleGenBtn: document.querySelector('.b-console__btn--generate'),
+      consoleClearBtn: document.querySelector('.b-console__btn--clear'),
+      init() {
+        myConsole.consoleGenBtn.addEventListener('click', myConsole.generateConsole)
+        myConsole.consoleClearBtn.addEventListener('click', () => {
+          console.clear()
+        })
+      },
+      generateConsole() {
+        // 帶樣式
+        console.log(
+          '%c JS30 Day9 Console 練習',
+          'font-size:25px; background: #212529; color: #fff;',
+        )
+  
+        // 一般常見
+        console.log('log', 'Day9 Console')
+        console.info('info', 'Style Work On FireFox')
+        console.warn('warn', 'Day9 Console')
+        console.error('error', 'Day9 Console ')
+  
+        // Interpolated  插入變數
+        // %s 表 string, %d或%i 表整數 %f表浮點數
+        console.log('Interpolated a %s string!', '💩')
+  
+        // Testing 用
+        // 判斷地一個參數是否為true, false則噴錯
+        // console.assert((1 > 2), '錯誤訊息: 1並非大於2')
+  
+        // console.dir 把物件展開
+        const consoleDemoObj = {
+          title: 'JS30 Day9 Console',
+        }
+        console.dir(consoleDemoObj)
+  
+        // console.table 可作欄位篩選
+        // Array 用 Table 形式呈現
+        const consoleDemoArray = [
+          { name: 'Snickers', age: 2 },
+          { name: 'hugo', age: 8 },
+        ]
+        console.table(consoleDemoArray, 'name')
+  
+        // timing 回傳
+        console.time('fetching data')
+        fetch('https://api.github.com/users/wesbos')
+          .then(data => data.json())
+          .then((data) => {
+            console.log(data)
+            console.timeEnd('fetching data')
+          })
+      },
     }
+    myConsole.init()
+
+
 
     /* Day10 Hold Shift and Check Checkboxes */
+
     /* Day11 Custom Video Player */
+    /* Day28 Video Speed Controller */
+
     /* Day12 Key Sequence Detection */
     /* Day13 Slide in on Scroll */
     /* Day14 JavaScript References VS Copying */
@@ -86,21 +282,7 @@
     /* Day25 Event Capture, Propagation, Bubbling and Once */
     /* Day26 Stripe Follow Along Nav */
     /* Day27 Click and Drag */
-    /* Day28 Video Speed Controller */
     /* Day29 Countdown Timer */
     /* Day30 Whack A Mole */
-
-
-
-    
-
-
   })
 }())
-    
-    
-
-
-
-
-
